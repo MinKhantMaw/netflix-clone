38ba1759fc21dc42395d8e92d7414fe6d102d12f
mkhant <minkhantmaw@onenex.co>
2024-11-20 22:40:18 +0630

    added: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import {Routes, Route} from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
+
 const App = () => {
   return (
     <div>
+        <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/login' element={<Login/>}/>
+        </Routes>
       <Home></Home>
     </div>
   );
